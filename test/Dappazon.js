030aac522a96e5ad2f7ce08cfc62026ae702e0f4
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-12-11 22:20:13 +0300

    test issue

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,17 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
-
+  it("Has a name", async () =>{
+    // deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+    const name = await dappazon.name
+    console.log(name)
+    // expect(await dappazon.name()).to.equal("Dappazon")
+  })
 })
