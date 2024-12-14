70bd42954925901a6124056aa167db9c1079001c
Boğaçhan Yiğitbaşı <by@concordium.com>
2024-12-14 09:30:10 +0300

    tests

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,5 +2,50 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
-    string public name = "Dappazon";
+    string public name;
+
+    address public owner;
+
+    constructor() {
+        name = "Dappazon";
+        owner = msg.sender;
+    }
+
+    struct Item {
+        uint256 tokenId;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+    //list products
+
+    function list(
+        uint256 _tokenId,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        Item memory item = Item(
+            _tokenId,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+        items[_tokenId] = item;
+    }
+
+    // buy products
+
+    // withdraw funds
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -5,13 +5,47 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+
+
 describe("Dappazon", () => {
-  it("Has a name", async () =>{
+  let dappazon;
+  let  deployer, buyer;
+
+
+  beforeEach(async() => {
+  // Setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    console.log("intial deployer", deployer.address)
     // deploy contract
     const Dappazon = await ethers.getContractFactory("Dappazon")
     dappazon = await Dappazon.deploy()
-    const name = await dappazon.name
-    console.log(name)
-    // expect(await dappazon.name()).to.equal("Dappazon")
   })
+
+
+  describe("Deployment",() => {
+    it("Has a name", async () =>{
+      const name = await dappazon.name
+      // console.log(name)
+      expect(await dappazon.name()).to.equal("Dappazon")
+    })
+    it("Deployer", async () =>{
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+  })
+
+  describe("Listing", ()=>{
+
+    beforeEach(async()=>{
+      const transaction = await dappazon.connect(deployer).list(1, "gazelle", "shoes", "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg", tokens(1),5,tokens(0.1), {gasLimit:300000})
+      await transaction.wait()
+    })
+    it("Is item listed", async ()=>{
+      const it = await dappazon.items(1)
+      console.log(it)
+      let name = it.name
+      console.log(name)
+      expect(name).equal('gazelle')
+  })
+})
 })
